2832b3687d68362e7926e620d5025a228cb8a6a0
Nirbhay <da201501068@gmail.com>
2020-08-02 20:08:07 +0530

    first graphql query executed

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import Link from './Link'
+import { useQuery } from 'urql'
+import gql from 'graphql-tag'
 
-const linksToRender = [
-    {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎',
-        url: 'https://www.prismagraphql.com',
-    },
-    {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://formidable.com/open-source/urql/',
-    },
-]
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
 
-const LinkList = () => (
+
+const LinkList = () => {
+  const [result] = useQuery({ query: FEED_QUERY })
+  const { data, fetching, error } = result
+  
+  if (fetching) return <div>Fetching</div>
+  if (error) return <div>Error</div>
+  
+  const linksToRender = data.feed.links
+
+  return (
     <div>
-        {linksToRender.map(link =>
-            <Link key={link.id} link={link} />
-        )}
+      {linksToRender.map(link => <Link key={link.id} link={link} />)}
     </div>
-)
+  );
+};
 
 export default LinkList
