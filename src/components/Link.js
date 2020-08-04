948119710bf48e2a9466a9ce908996180502860b
Nirbhay <da201501068@gmail.com>
2020-08-04 23:08:21 +0530

    added voting mutation

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { getToken } from '../token'
+import { timeDifferenceForDate } from '../dates'
+import gql from 'graphql-tag'
+import { useMutation } from 'urql'
 
-const Link = ({ link }) => (
-  <div>
-    <div>
-      {link.description} ({link.url})
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const Link = ({ index, link }) => {
+  const isLoggedIn = !!getToken()
+
+  const [state, executeMutation] = useMutation(VOTE_MUTATION);
+
+  const upvote = React.useCallback(() => {
+    if (!state.fetching) {
+      executeMutation({ linkId: link.id });
+    }
+  }, [state.fetching, executeMutation, link.id]);
+
+  return (
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {isLoggedIn && (
+          <div className="ml1 gray f11" onClick={upvote}>
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{' '}
+          {link.postedBy
+            ? link.postedBy.name
+            : 'Unknown'}{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Link
