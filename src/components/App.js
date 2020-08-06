43f23ecfa0bf11bdef0d8337353375afb564a56b
Nirbhay <da201501068@gmail.com>
2020-08-06 21:41:36 +0530

    added route for pagination

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,9 @@ const App = () => (
 		<Header />
 		<div className="ph3 pv1 background-gray">
 			<Switch>
-				<Route exact path="/" component={LinkList} />
+				<Route exact path="/" render={() => <Redirect to="/new/1" />} />
+				<Route exact path="/top" component={LinkList} />
+				<Route exact path="/new/:page" component={LinkList} />
 				<Route exact path="/create" component={CreateLink} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/search" component={Search} />
